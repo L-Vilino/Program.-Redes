dd8b9283cd2bfd4d33fd33bb804666565a769bb9
Liedson G. V. de Aquino <l.g.v.a.1700@gmail.com>
2024-11-13 03:42:04 +0000

    Pending changes exported from your codespace

---
+++ Atividades/08-11/Q4.py
@@ -0,0 +1,28 @@
+from datetime import date
+'''
+O número de dias decorridos entre duas datas é importante em uma infinidade de
+situações da vida cotidiana. Faça um programa que recebe inicialmente dois valores (dia inicial e mês
+inicial), depois mais dois valores (dia final, mês final), ao final deve indicar quantos dias se passaram entre
+as datas (considere que o ano não é bissexto).
+Exemplos:
+o 02 de 05 até 03 de 05 - 1 dia
+o 27 de 04 até 03 de 05 - 6 dias
+o 03 de 02 até 03 de 05 - 79 dias
+Não esqueça de verificar se a data inicial é menor ou igual à data final.
+Dica: conte o número de dias até cada uma das datas e subtraia esses números
+'''
+
+dia_i = int(input('Digite o dia inicial: '))
+mes_i = int(input('Digite o mês inicial: '))
+dia_f = int(input('Digite o dia final: '))
+mes_f = int(input('Digite o mês final: '))
+
+data_i = date(2012,mes_i,dia_i)
+data_f = date(2012,mes_f,dia_f)
+
+dias_corridos = data_f - data_i
+
+if data_i <= data_f:
+    print('A data inicial tem que ser menor que a data final')
+
+print(dias_corridos)
